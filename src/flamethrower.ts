f0b18bd26d0cf48e9c65e107aa69a921945ddb86
gregwoodsfrontier <iwannaplaygamesjp@gmail.com>
2024-09-06 00:55:31 -0400

    added render rect in flamethrowe

--- src/flamethrower.ts
+++ src/flamethrower.ts
@@ -1,13 +1,9 @@
-import { EngineObject, Vector2, vec2, tile, Timer, randInt } from "littlejsengine";
-import { TILEMAP_LOOKUP } from "./level";
-import { makeDeb, PALLETE } from "./effects";
+import { EngineObject, Vector2, vec2, Timer, randInt, drawRect, rgb } from "littlejsengine";
 import Fireball from "./fireball";
 
 export default class Flamethrower extends EngineObject {
-    constructor(_parent: EngineObject, _cdPeriod: number) {
-        const offsetVec = _parent.mirror ? vec2(-0.5, 0) : vec2(0.5, 0)
-        super(_parent.pos.add(offsetVec))
-        this.parent = _parent
+    constructor(_pos: Vector2, _cdPeriod: number) {
+        super(_pos)
         this.cooldownTimer = new Timer()
         this.cdPeriod = _cdPeriod
     }
@@ -21,16 +17,22 @@ export default class Flamethrower extends EngineObject {
     }
 
     posUpdate() {
-        const offsetVec = this.parent.mirror ? vec2(-0.5, 0) : vec2(0.5, 0)
-        this.pos = (this.parent as EngineObject).pos.copy().add(offsetVec)
+        this.localPos = (this.parent as EngineObject).mirror ? vec2(-0.5, 0) : vec2(0.5, 0)
+    }
+
+    render(): void {
+        drawRect(this.localPos, vec2(0.4, 0.4), rgb(1, 1, 0, 0.5))
     }
 
     spawnFire() {
+        if(this.cooldownTimer.active()) return
+
         if((this.parent as EngineObject).mirror) {
             new Fireball(this.pos).velocity = vec2(-1, randInt(-3, 3)).normalize(3)
         } else {
             new Fireball(this.pos).velocity = vec2(1, randInt(-3, 3)).normalize(3)
         }
+        this.cooldownTimer.set(this.cdPeriod)
     }
 
 }
